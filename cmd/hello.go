4800e770f032b8cf523a90c1a08b9b0c45a093a5
Anthony Hayes <110310587@umail.ucc.ie>
2023-08-05 21:12:57 +0000

    changes

--- cmd/hello.go
+++ cmd/hello.go
@@ -21,7 +21,7 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("hello called")
-		dothing := 	exec.Command("go", "version")
+		dothing := exec.Command("go", "version")
 		stdout, _ := dothing.Output()
 		fmt.Println(string(stdout))
 	},
